295674c1b348001dff4d27d9fa15006f96349948
Stephen <schematic31@gmail.com>
2018-03-04 11:40:11 -0800

    Added bars to display cpu usage % for each cpu

--- resmonitor/resmonitor.py
+++ resmonitor/resmonitor.py
@@ -1,10 +1,13 @@
 import tkinter as tk
+from tkinter.ttk import Progressbar
+from tkinter import HORIZONTAL
 
 import psutil
 
 
 class Application(tk.Frame):
     CPU_USAGE_UPDATE_TIME_MS = 1000
+    NUM_CPUS = psutil.cpu_count()
 
     def __init__(self, master: tk.Tk = None):
         super().__init__(master)
@@ -13,6 +16,11 @@ class Application(tk.Frame):
                               command=master.destroy)
         self.cpu_percent = tk.Label(self)
 
+        self.cpu_list = []
+        for i in range(self.NUM_CPUS):
+            new_cpu = Progressbar(self, orient=HORIZONTAL, length=100, mode='determinate')
+            self.cpu_list.append(new_cpu)
+
         self.pack()
         self.setup_widgets()
 
@@ -20,10 +28,18 @@ class Application(tk.Frame):
         self.cpu_percent["text"] = psutil.cpu_percent(percpu=True)
         self.cpu_percent.pack(side="top")
 
+        for i in range(self.NUM_CPUS):
+            self.cpu_list[i].pack()
+
         self.quit.pack(side="bottom")
 
     def update_cpu_usage(self):
-        self.cpu_percent["text"] = psutil.cpu_percent(percpu=True)
+        cpu_percent = psutil.cpu_percent(percpu=True)
+        self.cpu_percent["text"] = cpu_percent
+
+        for i in range(self.NUM_CPUS):
+            self.cpu_list[i]['value'] = cpu_percent[i]
+
         self.master.after(self.CPU_USAGE_UPDATE_TIME_MS, self.update_cpu_usage)
 
 
