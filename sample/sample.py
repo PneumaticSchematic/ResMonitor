148fd191eabf9e63295ca2ad8f2f442db52d0d72
Sam Burba <gitlab@samburba.com>
2018-03-04 09:38:21 -0500

    Define instance attributes inside of init

--- sample/sample.py
+++ sample/sample.py
@@ -6,16 +6,18 @@ import psutil
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tk.Label(self)
-        self.hi_there["text"] = psutil.cpu_percent(percpu=True)
-        self.hi_there.pack(side="top")
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=root.destroy)
+        self.hi_there = tk.Label(self)
+
+        self.pack()
+        self.setup_widgets()
+
+    def setup_widgets(self):
+        self.hi_there["text"] = psutil.cpu_percent(percpu=True)
+        self.hi_there.pack(side="top")
+
         self.quit.pack(side="bottom")
 
     def update_cpu_usage(self):
