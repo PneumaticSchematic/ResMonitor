9c02a1836dffa3d9d54f1d9a76278026803f36a2
Stephen <schematic31@gmail.com>
2018-03-14 19:03:41 -0700

    Bug fix + window doesn't look stupid in i3

--- resmonitor/resmonitor.py
+++ resmonitor/resmonitor.py
@@ -17,21 +17,27 @@ class Application(tk.Frame):
                               command=master.destroy)
         self.cpu_percent = tk.Label(self)
 
-        self.cpu_list = []
+        self.cpu_labels = []
+        self.cpu_bars = []
         for i in range(self.num_cpus):
             new_cpu = ttk.Progressbar(self, orient=tk.HORIZONTAL, length=100,
                                       mode='determinate', style="blue.Horizontal.TProgressbar")
-            self.cpu_list.append(new_cpu)
+            self.cpu_bars.append(new_cpu)
+
+            new_label = tk.Label(self)
+            self.cpu_labels.append(new_label)
 
         self.pack()
         self.setup_widgets()
 
     def setup_widgets(self):
-        self.cpu_percent["text"] = self.proc_info.cpu_percent(percpu=True)
+        cpu_percent = self.proc_info.cpu_percent(percpu=True)
         self.cpu_percent.pack(side="top")
 
         for i in range(self.num_cpus):
-            self.cpu_list[i].pack()
+            self.cpu_bars[i].pack()
+            self.cpu_labels[i].pack()
+            self.cpu_labels[i]["text"] = cpu_percent[i]
 
         self.quit.pack(side="bottom")
 
@@ -40,7 +46,8 @@ class Application(tk.Frame):
         self.cpu_percent["text"] = cpu_percent
 
         for i in range(self.num_cpus):
-            self.cpu_list[i]['value'] = cpu_percent[i]
+            self.cpu_bars[i]['value'] = cpu_percent[i]
+            self.cpu_labels[i]["text"] = cpu_percent[i]
 
         self.master.after(self.CPU_USAGE_UPDATE_TIME_MS, self.update_cpu_usage)
 
@@ -51,6 +58,8 @@ if __name__ == '__main__':
         master=root,
         proc_info=psutil
     )
+
+    root.attributes('-type', 'dialog')
 
     s = ttk.Style()
     s.theme_use('clam')
