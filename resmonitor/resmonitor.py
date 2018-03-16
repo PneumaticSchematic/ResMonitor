dc13e30b0b459030b23d178c2cd1f9dd1e66b34b
Stephen <schematic31@gmail.com>
2018-03-16 16:51:47 -0700

    Added names for each CPU

--- resmonitor/resmonitor.py
+++ resmonitor/resmonitor.py
@@ -18,13 +18,17 @@ class Application(tk.Frame):
 
         self.cpu_labels = []
         self.cpu_bars = []
+        self.cpu_names = []
         for i in range(self.num_cpus):
-            new_cpu = ttk.Progressbar(self, orient=tk.HORIZONTAL, length=100,
-                                      mode='determinate', style="blue.Horizontal.TProgressbar")
-            self.cpu_bars.append(new_cpu)
+            new_cpu_bar = ttk.Progressbar(self, orient=tk.HORIZONTAL, length=100,
+                                           mode='determinate', style="blue.Horizontal.TProgressbar")
+            self.cpu_bars.append(new_cpu_bar)
 
-            new_label = tk.Label(self)
-            self.cpu_labels.append(new_label)
+            new_cpu_label = tk.Label(self)
+            self.cpu_labels.append(new_cpu_label)
+
+            new_cpu_name = tk.Label(self)
+            self.cpu_names.append(new_cpu_name)
 
         self.pack()
         self.setup_widgets()
@@ -33,9 +37,11 @@ class Application(tk.Frame):
         cpu_percent = self.proc_info.cpu_percent(percpu=True)
 
         for i in range(self.num_cpus):
-            self.cpu_bars[i].grid(row=i * 2, columnspan=4)
-            self.cpu_labels[i].grid(row=i * 2 + 1, column=2)
-            self.cpu_labels[i]["text"] = cpu_percent[i]
+            self.cpu_bars[i].grid(row=i*2, columnspan=4)
+            self.cpu_labels[i].grid(row=i*2+1, column=2)
+            self.cpu_labels[i]['text'] = cpu_percent[i]
+            self.cpu_names[i].grid(row=i*2+1, column=0)
+            self.cpu_names[i]['text'] = "CPU%d" % i
 
         self.quit.grid(row=9, column=0)
 
