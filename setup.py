94207d91b37c7fe62d1bf1074a6963e4498356f2
Stephen <schematic31@gmail.com>
2018-05-13 12:27:04 -0700

    Changed repository name in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
         name = 'coremeter',
         packages = ['coremeter'],
-        version = '1.0.0',
+        version = '1.0.1',
         description = 'A resource monitor for Unix',
         author = 'Stephen Johnson',
         author_email = 'stephen@sjoh.net',
         license='MIT',
-        url = 'https://github.com/S-M-Johnson/resmonitor',
-        download_url = 'https://github.com/S-M-Johnson/resmonitor/archive/1.0.0.tar.gz',
+        url = 'https://github.com/S-M-Johnson/coremeter',
+        download_url = 'https://github.com/S-M-Johnson/coremeter/archive/1.0.0.tar.gz',
         keywords = 'cpu resource monitor unix',
         classifiers = [],
     )
